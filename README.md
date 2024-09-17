601e86c693ce5f9a0ea146b2c834ab317f3eaec6
Samuel Gómez <samuel.gomez.balderas@gmail.com>
2024-09-17 13:17:25 -0600

    simple command

---
+++ README.md
@@ -0,0 +1,4 @@
+This little guide shows you how to use this tool
+
+- First install this package: `pip install -e .`
+- Then use this command to get help: `greetings --help`
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='yourscript',
+    version='0.1.0',
+    py_modules=['my_module'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'greetings = my_module:hello',
+        ],
+    },
+)
